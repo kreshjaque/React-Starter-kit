ae81a61a37788e11b5f87bd643cc8f97804468af
Krishna Kumar <kreshjaque@google.com>
2022-01-19 04:20:05 +0530

    Added the chalk plugin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import chalk from 'chalk';
 import { Title, Content } from './App.styles';
 
-const App = () => (
+const App = () => { 
+  console.log(chalk.blue('Hello world!'));
+  return (
     <div>
         <Title>Welcome to React App</Title>
         <Content>Date :{new Date().toDateString()}</Content>
     </div>
-);
+)};
 
 export default App;
